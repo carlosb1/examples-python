d61d02f9710b957623097acdfe1360e9ac091b8f
carlosb <carlos.baezruiz@gmail.com>
2016-01-27 09:07:20 +0100

    Added first part of the recursive implementation to order all possibility words

--- example2/find_words.py
+++ example2/find_words.py
@@ -1,6 +1,42 @@
 
-"""
-Function to find words
-"""
-def find_words(list_of_words):
-	return 1
+class WordFinder(object):
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
+
+    """
+    Function to find words
+    """
+    def find(list_of_letters):
+        #TODO check words   
+        if list_of_letters == None:
+            return []
+        set_of_find_words = []
+        recur_find(list_of_letters,set_of_find_words) 
+        return set_of_find_words
+
+        
+    def recur_find(list_of_words,set_of_find_words):
+        if len(list_of_words) == 0 :
+            return True
+        for index_letters in range(1,len(list_of_words)):
+            letters = list_of_words[0:index_letters]
+            possible_words= create_possible_words(letters)
+            if len(possible_words):
+                return False
+            #Append por insert
+            for word in possible_words:
+                set_of_find_words.append(word)
+                #Check index
+                result = recur_find(list_of_words[index_letters:],set_of_find_words)
+                #It is a correct word, it is not necessary continue searching
+                if result: 
+                    return True
+            
+
+
+        #letters_to_setup = list_of_words[0]
+        #possible words
+        #possible_words = [ for ] 
+
+        return True   
+
--- example2/test_find_words.py
+++ example2/test_find_words.py
@@ -1,16 +1,49 @@
 import unittest
 from find_words import find_words
 import os
+import string
 
 
 
-class TestFindWords(unittest.TestCase):
-    #TODO check all necessary tests
+
+def load_dictionary(filename):
+	import re
+	dictionary={}
+	with open(filename) as f:
+		content = f.readlines()
+	
+	for word in content:
+		word = re.sub('[^a-zA-Z0-9-_*.]','',word)	
+		dictionary[word]=word
+
+	return dictionary
+
+def generate_random_letters(siz_letters):
+	import string
+	import random
+	values = [random.choice(string.lowercase) for x in range(0,siz_letters)]
+	return values
+
+
     #Acceptance tests
     #Unit tests
     #Functional tests
-    def test_normal_case(self):
-            pass
+class TestFindWords(unittest.TestCase):
+	def test_load_dictionary(self):
+		dic = load_dictionary("./dictionary.txt")
+		self.assertTrue(len(dic) > 0)
+	def test_generate_random_letters(self):
+		values = generate_random_letters(10)
+		print values
+		self.assertTrue(len(values) > 0)
+
+	def test_normal_case(self):
+		dictionary = load_dictionary("./dictionary.txt")
+		values = generate_random_letters(30)
+		phrase = find_words(dictionary,)
+
+
+		pass
 
 if __name__ == '__main__':
 	unittest.main()
