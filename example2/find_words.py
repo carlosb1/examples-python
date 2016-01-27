4a5ba13c6778a01d50addbe8b1ec88e419aced82
carlosb <carlos.baezruiz@gmail.com>
2016-01-27 09:30:42 +0100

    Added version, pending to test for find_words

--- example2/find_words.py
+++ example2/find_words.py
@@ -11,32 +11,27 @@ class WordFinder(object):
         if list_of_letters == None:
             return []
         set_of_find_words = []
-        recur_find(list_of_letters,set_of_find_words) 
-        return set_of_find_words
+        result = recur_find(list_of_letters,set_of_find_words) 
+        return result[1]
 
         
     def recur_find(list_of_words,set_of_find_words):
         if len(list_of_words) == 0 :
-            return True
+            return [True,list_of_words]
         for index_letters in range(1,len(list_of_words)):
             letters = list_of_words[0:index_letters]
             possible_words= create_possible_words(letters)
             if len(possible_words):
-                return False
+                return [False,list_of_words]
             #Append por insert
             for word in possible_words:
                 set_of_find_words.append(word)
                 #Check index
                 result = recur_find(list_of_words[index_letters:],set_of_find_words)
                 #It is a correct word, it is not necessary continue searching
-                if result: 
-                    return True
-            
+                if result[0]: 
+                    return [True,list_of_words]
 
 
-        #letters_to_setup = list_of_words[0]
-        #possible words
-        #possible_words = [ for ] 
-
-        return True   
+        return [False, list_of_words]
 
