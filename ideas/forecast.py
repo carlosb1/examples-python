d42a58ecd4f304e8f2dd51fcd8510dc1d4bdfe6c
carlosb <carlos.baezruiz@gmail.com>
2016-02-01 15:43:50 +0100

    Trying to add ORM query to check values

--- ideas/forecast.py
+++ ideas/forecast.py
@@ -26,13 +26,29 @@ class InfoSpot(Base):
     wind_direction = Column(Integer)
     wind_compass_direction = Column(String)
     wind_speed = Column(Integer)
-    wind_type_speed = Column(String)
+    #wind_type_speed = Column(String)
     wind_chill = Column(Integer)
     wind_unit = Column(String)
     local_timestamp = Column(String)
     id_spot=Column(String)
 
+    def __init__(self):
+        result = "id="+str(id)
+        +" id_spot="+str(id_spot)
+        +" solid_rating="+str(solid_rating)
+        +" faded_rating="+str(faded_rating)
+        +" chart_wind_url="+str(chart_wind_url)
+        +" chart_period_url="+str(chart_period_url)
+        +" wind_direction="+str(wind_direction)
+        +" wind_compass_direction="+str(wind_compass_direction)
+        +" wind_speed="+str(wind_speed)
+        #+" wind_type_speed="+str(wind_type_speed)
+        +" wind_chill="+str(wind_chill)
+        +" wind_unit="+str(wind_unit)
+        +" local_timestamp="+str(local_timestamp)
 
+
+        return result
 
 def timestamp2String(timestamp):
     import datetime
@@ -67,22 +83,22 @@ class ApiReader(object):
 	    #db_ins.execute()
 	
 	    
-	    print "time: "+timestamp2String(local_timestamp)+" solidRating="+str(solid_rating)+" fadedRating="+str(faded_rating)
-	    print "chart_wind: "+str(chart_wind_url)
-	    print "chart_period: "+str(chart_period_url)
+	#    print "time: "+timestamp2String(local_timestamp)+" solidRating="+str(solid_rating)+" fadedRating="+str(faded_rating)
+	#    print "chart_wind: "+str(chart_wind_url)
+	#    print "chart_period: "+str(chart_period_url)
 	   # print "chart_sst: "+str(chart_sst)
-	    print "wind_direction: "+str(wind_direction)+" wind_compass_direction: "+wind_compass_direction
-	    print "wind_speed: "+str(wind_speed)+ " "+str(wind_unit)
-	    print "wind_chill: "+str(wind_chill)
+	#    print "wind_direction: "+str(wind_direction)+" wind_compass_direction: "+wind_compass_direction
+	#    print "wind_speed: "+str(wind_speed)+ " "+str(wind_unit)
+	#    print "wind_chill: "+str(wind_chill)
 	
-	    spot = InfoSpot(local_timestamp=local_timestamp,solid_rating=solid_rating,faded_rating=faded_rating
-	            ,chart_wind_url=chart_wind_url,chart_period_url=chart_period_url,wind_direction=wind_direction
-	            ,wind_compass_direction=wind_compass_direction, wind_unit=wind_unit
-	            ,wind_chill=wind_chill, wind_speed=wind_speed, id_spot=id_spot)
+	    spot = InfoSpot(solid_rating=solid_rating,faded_rating=faded_rating,
+                    chart_wind_url=chart_wind_url,chart_period_url=chart_period_url,wind_direction=wind_direction,wind_compass_direction=wind_compass_direction,wind_speed=wind_speed,
+                    wind_chill=wind_chill, wind_unit=wind_unit,local_timestamp=local_timestamp,id_spot=self.id_spot)
+            #It only adds
 
-            #print self.session.query(exists().where(InfoSpot.local_timestamp = local_timestamp)).scalar()
-	    self.session.add(spot)
-	    self.session.commit()
+            if not self.session.query(exists().where(InfoSpot.local_timestamp == local_timestamp).where(InfoSpot.id_spot == self.id_spot)).scalar():
+	        self.session.add(spot)
+	        self.session.commit()
 
 #Read file configuration
 fname = "secret_forecast"
