7390a75c5e1d123292cabf61971d9d605fda1e60
carlosb <carlos.baezruiz@gmail.com>
2016-02-01 09:05:37 +0100

    Added first celery version, pending to add checking version

--- ideas/forecast.py
+++ ideas/forecast.py
@@ -11,6 +11,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
+from sqlalchemy.sql import exists
+
 Base = declarative_base()
 
 #define object
@@ -28,6 +30,7 @@ class InfoSpot(Base):
     wind_chill = Column(Integer)
     wind_unit = Column(String)
     local_timestamp = Column(String)
+    id_spot=Column(String)
 
 
 
@@ -35,7 +38,51 @@ def timestamp2String(timestamp):
     import datetime
     return datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
 
+import requests
 
+#API Reader
+class ApiReader(object):
+    def __init__(self,key,id_spot,session):
+        self.id_spot = id_spot
+        self.key = key
+        self.url = "http://magicseaweed.com/api/"+str(self.key)+"/forecast/?spot_id="+str(self.id_spot)+"&units=eu&fields=timestamp,localTimestamp,fadedRating,solidRating,threeHourTimeText,wind.*,condition.temperature,charts.*"
+        self.session = session
+
+    def update(self):
+        r = requests.get(self.url)
+        json_values = r.json()
+	for value in json_values:
+	    #Parse times
+	    local_timestamp = value["localTimestamp"]
+	    solid_rating = value["solidRating"]
+	    faded_rating = value["fadedRating"]
+	    chart_wind_url =  value["charts"]["wind"]
+	    chart_period_url =  value["charts"]["period"]
+	   # chart_sst =  value["charts"]["sst"] #Honda
+	    wind_direction = value["wind"]["direction"]
+	    wind_compass_direction = value["wind"]["compassDirection"] #Brujula
+	    wind_speed = value["wind"]["speed"]
+	    wind_unit = value["wind"]["unit"]
+	    wind_chill = value["wind"]["chill"] #Frio
+	    #db_ins.execute()
+	
+	    
+	    print "time: "+timestamp2String(local_timestamp)+" solidRating="+str(solid_rating)+" fadedRating="+str(faded_rating)
+	    print "chart_wind: "+str(chart_wind_url)
+	    print "chart_period: "+str(chart_period_url)
+	   # print "chart_sst: "+str(chart_sst)
+	    print "wind_direction: "+str(wind_direction)+" wind_compass_direction: "+wind_compass_direction
+	    print "wind_speed: "+str(wind_speed)+ " "+str(wind_unit)
+	    print "wind_chill: "+str(wind_chill)
+	
+	    spot = InfoSpot(local_timestamp=local_timestamp,solid_rating=solid_rating,faded_rating=faded_rating
+	            ,chart_wind_url=chart_wind_url,chart_period_url=chart_period_url,wind_direction=wind_direction
+	            ,wind_compass_direction=wind_compass_direction, wind_unit=wind_unit
+	            ,wind_chill=wind_chill, wind_speed=wind_speed, id_spot=id_spot)
+
+            #print self.session.query(exists().where(InfoSpot.local_timestamp = local_timestamp)).scalar()
+	    self.session.add(spot)
+	    self.session.commit()
 
 #Read file configuration
 fname = "secret_forecast"
@@ -59,58 +106,15 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+
+celery = Celery('forecast')
+celery.config_from_object('celeryconfig')
+
 #Defined spots
 idBarceloneta = 3535
+reader = ApiReader(key,idBarceloneta,session)
 
-
-
-
-
-#API Reader
-import requests
-
-r = requests.get("http://magicseaweed.com/api/"+str(key)
-        +"/forecast/?spot_id="+str(idBarceloneta)
-        +"&units=eu"
-        +"&fields=timestamp,localTimestamp,"
-        +"fadedRating,solidRating,threeHourTimeText,"
-        +"wind.*,condition.temperature,"
-        +"charts.*")
-
-json_values = r.json()
-
-
-for value in json_values:
-    #Parse times
-    local_timestamp = value["localTimestamp"]
-    solid_rating = value["solidRating"]
-    faded_rating = value["fadedRating"]
-    chart_wind_url =  value["charts"]["wind"]
-    chart_period_url =  value["charts"]["period"]
-   # chart_sst =  value["charts"]["sst"] #Honda
-    wind_direction = value["wind"]["direction"]
-    wind_compass_direction = value["wind"]["compassDirection"] #Brujula
-    wind_speed = value["wind"]["speed"]
-    wind_unit = value["wind"]["unit"]
-    wind_chill = value["wind"]["chill"] #Frio
-    #db_ins.execute()
-
-    
-    print "time: "+timestamp2String(local_timestamp)+" solidRating="+str(solid_rating)+" fadedRating="+str(faded_rating)
-    print "chart_wind: "+str(chart_wind_url)
-    print "chart_period: "+str(chart_period_url)
-   # print "chart_sst: "+str(chart_sst)
-    print "wind_direction: "+str(wind_direction)+" wind_compass_direction: "+wind_compass_direction
-    print "wind_speed: "+str(wind_speed)+ " "+str(wind_unit)
-    print "wind_chill: "+str(wind_chill)
-
-    spot = InfoSpot(local_timestamp=local_timestamp,solid_rating=solid_rating,faded_rating=faded_rating
-            ,chart_wind_url=chart_wind_url,chart_period_url=chart_period_url,wind_direction=wind_direction
-            ,wind_compass_direction=wind_compass_direction, wind_unit=wind_unit
-            ,wind_chill=wind_chill, wind_speed=wind_speed)
-
-    session.add(spot)
-    session.commit()
-
-
-
+@celery.task
+def search_spot():
+    print "Calling searching spot"
+    reader.update()
