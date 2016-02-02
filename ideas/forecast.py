dcdcd79a39f8354d0a7ff9f3fc42f49409398304
carlosb <carlos.baezruiz@gmail.com>
2016-02-02 08:33:38 +0100

    Fixed problem to save python entity

--- ideas/forecast.py
+++ ideas/forecast.py
@@ -32,7 +32,7 @@ class InfoSpot(Base):
     local_timestamp = Column(String)
     id_spot=Column(String)
 
-    def __init__(self):
+    def __str__(self):
         result = "id="+str(id)
         +" id_spot="+str(id_spot)
         +" solid_rating="+str(solid_rating)
@@ -80,22 +80,25 @@ class ApiReader(object):
 	    wind_speed = value["wind"]["speed"]
 	    wind_unit = value["wind"]["unit"]
 	    wind_chill = value["wind"]["chill"] #Frio
-	    #db_ins.execute()
 	
 	    
-	#    print "time: "+timestamp2String(local_timestamp)+" solidRating="+str(solid_rating)+" fadedRating="+str(faded_rating)
-	#    print "chart_wind: "+str(chart_wind_url)
-	#    print "chart_period: "+str(chart_period_url)
-	   # print "chart_sst: "+str(chart_sst)
-	#    print "wind_direction: "+str(wind_direction)+" wind_compass_direction: "+wind_compass_direction
-	#    print "wind_speed: "+str(wind_speed)+ " "+str(wind_unit)
-	#    print "wind_chill: "+str(wind_chill)
+	    print "time: "+timestamp2String(local_timestamp)+" solidRating="+str(solid_rating)+" fadedRating="+str(faded_rating)
+	    print "chart_wind: "+str(chart_wind_url)
+	    print "chart_period: "+str(chart_period_url)
+	    print "wind_direction: "+str(wind_direction)+" wind_compass_direction: "+wind_compass_direction
+	    print "wind_speed: "+str(wind_speed)+ " "+str(wind_unit)
+	    print "wind_chill: "+str(wind_chill)
 	
 	    spot = InfoSpot(solid_rating=solid_rating,faded_rating=faded_rating,
-                    chart_wind_url=chart_wind_url,chart_period_url=chart_period_url,wind_direction=wind_direction,wind_compass_direction=wind_compass_direction,wind_speed=wind_speed,
-                    wind_chill=wind_chill, wind_unit=wind_unit,local_timestamp=local_timestamp,id_spot=self.id_spot)
-            #It only adds
-
+                    chart_wind_url=chart_wind_url,chart_period_url=chart_period_url,
+                    wind_direction=wind_direction,
+                    wind_compass_direction=wind_compass_direction,
+                    wind_speed=wind_speed,
+                    wind_chill=wind_chill,
+                    wind_unit=wind_unit,
+                    local_timestamp=local_timestamp,
+                    id_spot=self.id_spot
+                    )
             if not self.session.query(exists().where(InfoSpot.local_timestamp == local_timestamp).where(InfoSpot.id_spot == self.id_spot)).scalar():
 	        self.session.add(spot)
 	        self.session.commit()
