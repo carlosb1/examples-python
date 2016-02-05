d1e31ac547aa6d72aa29d6b89d8fb761958afc39
Carlos B <carlos.baezruiz@gmail.com>
2016-02-05 21:16:21 +0100

    Trying to parse correctly the web

--- ideas/mango-example/mango.py
+++ ideas/mango-example/mango.py
@@ -6,4 +6,9 @@ sess = dryscrape.Session(base_url = 'http://shop.mango.com')
 sess.set_attribute('auto_load_images',False)
 sess.visit('/ES/m/hombre/prendas/todas/?m=coleccion')
 
-print sess.at_xpath("//*[@class=\"searchResultPrice\"]").text()
+print sess.at_xpath("//*").children()
+print "--------------------------"
+print sess.at_xpath("//*[contains(@class,\"searchResultPrice\")]/text()")
+
+#for price in sess.at_xpath("//*[contains(@class,\"searchResultPrice\")]"):
+#    print price
