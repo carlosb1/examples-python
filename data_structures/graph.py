8905c01aeadebba4cac310558fb14578b33521c3
carlosb1 <mcflurry0@gmail.com>
2017-11-26 12:09:34 +0100

    Added example to find path

---
+++ data_structures/graph.py
@@ -0,0 +1,25 @@
+def find_path(graph, start, end, path= []):
+    path = path + [start]
+    if start == end:
+        return path
+    if not graph.has_key(start):
+        return None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph,node,end,path)
+            if newpath: return newpath
+    return none
+
+
+
+
+graph = {'A': ['B','C'],
+        'B': ['C','D'],
+         'C': ['D'],
+         'D': ['C'],
+         'E': ['F'],
+         'F': ['C']
+         }
+
+result_path = find_path(graph,'A','D')
+print str(result_path)
