3d7bdc768076aa281374b2b73598aaa3bf2dfe77
carlosb1 <mcflurry0@gmail.com>
2017-11-26 22:56:07 +0100

    Added bfs iterative version

--- data_structures/graph.py
+++ data_structures/graph.py
@@ -1,25 +1,49 @@
 def find_path(graph, start, end, path= []):
     path = path + [start]
+    #is it the path
     if start == end:
         return path
+    #no, has more graphs to visit
     if not graph.has_key(start):
         return None
+
+    #has graphs, search adjs
     for node in graph[start]:
+        #if it is not included, search there
         if node not in path:
             newpath = find_path(graph,node,end,path)
             if newpath: return newpath
-    return none
+    return None
+
+
+
+def bfs(grap, start,end):
+        queue = []
+        queue.append([start])
+        while queue:
+            path = queue.pop(0)
+            node = path[-1]
+            if node == end:
+                return path
+            for adjacent in graph.get(node,[]):
+                new_path = list(path)
+                new_path.append(adjacent)
+                queue.append(new_path)
 
 
 
 
 graph = {'A': ['B','C'],
-        'B': ['C','D'],
+        'B': ['G','C','D'],
          'C': ['D'],
-         'D': ['C'],
+         'D': ['C','E'],
          'E': ['F'],
-         'F': ['C']
+         'F': ['C'],
+         'G': []
          }
 
 result_path = find_path(graph,'A','D')
 print str(result_path)
+
+result_path_v2 = bfs(graph,'A','G')
+print str(result_path_v2)
