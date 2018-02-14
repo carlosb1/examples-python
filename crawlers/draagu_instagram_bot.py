400f353cfb478522425f239328249fbfe7c85a2b
carlosb <mcflurry0@gmail.com>
2018-02-15 00:01:04 +0100

    Proves to save json messages

--- crawlers/draagu_instagram_bot.py
+++ crawlers/draagu_instagram_bot.py
@@ -6,7 +6,7 @@ import sys
 import json
 
 sys.path.append(os.path.join(sys.path[0], '../'))
-from instabot import instabot
+from instabot import Bot
 
 try:
     input = raw_input
@@ -18,16 +18,23 @@ def get_media_id_with_location_and_tags(new_bot, new_location, tags):
     max_id=''
     if new_bot.getLocationFeed(new_location['location']['pk'], maxid=max_id):
         location_feed = new_bot.LastJson
-        print(json.dumps(location_feed))
+        import json
+        strJson =json.dumps(location_feed)
+        print(strJson)
+        import datetime, time
+        st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        fil = open('dump'+st+'.json','w')
+        fil.write(strJson)
+        fil.close()
         print("------------------------")
-        for media in new_bot.filter_medias(location_feed["items"][:amount], quiet=True):
-            print(str(media))
+        #for media in new_bot.filter_medias(location_feed["items"][:amount], quiet=True):
+        #    print(str(media))
             #if bot.comment(media, MESSAGE):
-            counter += 1
-            pbar.update(1)
-            if not location_feed.get('next_max_id'):
-                return False
-            max_id = location_feed['next_max_id']
+       #     counter += 1
+       #     pbar.update(1)
+       #     if not location_feed.get('next_max_id'):
+       #         return False
+       #     max_id = location_feed['next_max_id']
             
     return media_ids
 
@@ -37,9 +44,7 @@ def comment_location_feed(new_bot, new_location, amount=0):
     with tqdm(total=amount) as pbar:
         while counter < amount:
             if new_bot.getLocationFeed(new_location['location']['pk'], maxid=max_id):
-                location_feed = new_bot.LastJson
-                import json
-                print(json.dumps(location_feed))
+
                 print("------------------------")
                 for media in new_bot.filter_medias(location_feed["items"][:amount], quiet=True):
                     print(str(media))
@@ -58,9 +63,7 @@ def comment_location_feed(new_bot, new_location, amount=0):
 parser = argparse.ArgumentParser(add_help=True)
 parser.add_argument('-u', type=str, help="username")
 parser.add_argument('-p', type=str, help="password")
-parser.add_argument('-p', type=str, help="password")
 parser.add_argument('tags', type=str, metavar='tag',nargs='+', help='message')
-parser.add_argument('message', type=str, nargs='*', help='message')
 args = parser.parse_args()
 
 
@@ -73,21 +76,26 @@ except TypeError:
 bot = Bot()
 bot.login(username=args.u, password=args.p)
 
-if not args.message:
-    print("It  is necessary to add some message to start the analysis")
-    return
-
+message = 'hello world'
 if not args.tags or len(args.tags)==0:
     print("It  is necessary to add tags to start the analysis")
-    return
+    exit(1)
+
 
 
 location_name = input(u"Write location name:\n").strip()
 
+print("msg:"+message)
+print("tags:"+str(args.tags))
+print("location:"+location_name)
+
+
+bot.searchLocation(location_name)
+
 if not bot.LastJson['items']:
     exit(1)
 
-ncomments = args.amount or input(u"How much comments per location?\n")
+ncomments =  input(u"How much comments per location?\n")
 ans = True
 
 media_ids=[]
@@ -105,9 +113,9 @@ while ans:
                 #print(json.dumps(bot.LastJson["items"]))
                 #print("---------------------")
                 #print(str(bot.LastJson["items"][ans]))
-                #print("#######################")
+                print("#######################")
 
-                get_media_id_with_location_and_tags(bot,bot.LastJson["items"][ans],tags)
+                get_media_id_with_location_and_tags(bot,bot.LastJson["items"][ans],args.tags)
     except ValueError:
         print(u"\n Not valid choice. Try again")
 
