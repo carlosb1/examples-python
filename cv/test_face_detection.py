812dc8b1cefe1be3de974325cb3bab7486472ea2
Carlos B <carlos.baezruiz@gmail.com>
2018-06-14 23:16:05 +0200

    Added test for read faces

--- cv/test_face_detection.py
+++ cv/test_face_detection.py
@@ -2,7 +2,7 @@ import glob
 import cv2 as cv
 import numpy as np
 
-path_images = './output/images'
+path_images = './joan_images_test'
 images = glob.glob(path_images+"/*.jpg")
 
 for path_image in images:
@@ -17,38 +17,42 @@ for path_image in images:
     print("name_image="+str(name_image))
 
     image = cv.imread(path_image)
-    height, width, channel = image.shape
-    face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-    #TODO check parameters and function
-    gray = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        padh = (int)(h * .40)
-        padw = (int)(w * .20)
-        y0 = y - padh
-        x0 = x - padw
-        yh1 = y+h + padh
-        xw1 = x+w + padw
-        if y0 < 0:
-            y0 = 0
-        if x0 < 0:
-            x0 = 0
-        if yh1 >=height:
-            yh1=height-1
-        if xw1>=width:
-            xw1=width-1
+    try: 
+        height, width, channel = image.shape
+        face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+        #TODO check parameters and function
+        gray = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        num_face = 0
+        for (x,y,w,h) in faces:
+            padh = (int)(h * .40)
+            padw = (int)(w * .20)
+            y0 = y - padh
+            x0 = x - padw
+            yh1 = y+h + padh
+            xw1 = x+w + padw
+            if y0 < 0:
+                y0 = 0
+            if x0 < 0:
+                x0 = 0
+            if yh1 >=height:
+                yh1=height-1
+            if xw1>=width:
+                xw1=width-1
+    
+            
+            import os
+            output_dir = path_fil+"/output_cropped/"
+            if not os.path.exists(output_dir):
+                os.mkdir(output_dir)
 
-        cv.rectangle(image,(x0,y0),(xw1,yh1),(0,255,0),2)
-        cv.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
-        
-        #roi_img = image[y:y+h, x:x+w] 
-        #roi_gray = gray[y:y+h, x:x+w]
-        cv.imshow('img',image)
-    key = cv.waitKey()
-    if key == ord('q'):
-        break
-
-cv.destroyAllWindows()
+     
+            roi_img = image[y0:yh1, x0:xw1]
+            cv.imwrite(output_dir+name_image+"_"+str(num_face)+"."+name_extension, roi_img)
+            
+            num_face+=1
+    except:
+        print("Skipping image: "+str(path_image))
 
 
 
