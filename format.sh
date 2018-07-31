cefa88e7da5e04b886b7dcd8a0c47fe689af1e82
Carlos B <carlos.baezruiz@gmail.com>
2018-07-31 11:33:27 +0200

    Added format for one file

--- format.sh
+++ format.sh
@@ -5,6 +5,17 @@ if (($# < 1)); then
 	exit;
 fi
 
-for filename in $1/*.py; do
-	yapf -i $filename && autoflake -i $filename;
-done
+
+if [[ -d "$1" ]]; then
+	for filename in $1/*.py; do
+		yapf -i $filename && autoflake -i $filename;
+	done
+elif  [[ -f "$1" ]]; then
+	yapf -i "$1" && autoflake -i "$1";
+else
+	echo "It is not correct input"
+	exit 1
+fi
+
+
+
