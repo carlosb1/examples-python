c69e5335f161e04875ee66dbaf1b86bda3f78798
Carlos B <carlos.baezruiz@gmail.com>
2019-05-02 16:30:34 +0200

    Updated project to permit CORS, deleted old project

--- flask-news2/api.py
+++ flask-news2/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from redis import Redis
 from rq import Queue
@@ -6,9 +7,17 @@ import logging
 
 from worker import runTask
 
+
+
 LOGGER = logging.getLogger()
 
 app = FastAPI()
+
+# CORS configuration
+origins = ["*"]
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True
+, allow_methods=["*"], allow_headers=["*"])
+
 
 redis_conn = Redis(host='redis', port=6379, db=0)
 q = Queue('my_queue', connection=redis_conn)
--- flask-news2/frontend/src/components/Ping.vue
+++ flask-news2/frontend/src/components/Ping.vue
@@ -16,7 +16,7 @@ export default {
   },
   methods: {
     getMessage() {
-      const path = 'http://hello.world/books/ping';
+      const path = 'http://0.0.0.0:5057/hello';
       axios.get(path)
         .then((res) => {
           this.msg = res.data;
